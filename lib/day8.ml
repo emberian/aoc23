b9cbf49d48c0fb5e3750a999cde40e9d3ccc7c2b
ember arlynx <ember@lunar.town>
2023-12-08 16:11:41 -0500

    restore the pt1 code, note the cycles are independent

--- lib/day8.ml
+++ lib/day8.ml
@@ -14,26 +14,29 @@ let parse_line g line =
   let r = Option.value_exn matches.(3) in
   Hashtbl.add g ~key:src ~data:(l, r) |> ignore
 
+let least_common_multiple (l : Z.t list) = List.fold_left ~f:Z.lcm ~init:Z.one l
+
 let go () =
   let input = In_channel.read_lines "input.txt" in
   let path =
     List.nth_exn input 0 |> String.to_list |> Sequence.cycle_list_exn
   in
-  let ct = ref Bigint.zero in
   let g = Graph.create () in
-  let q = ref [] in
+  let q = Queue.create () in
+  let finished = Queue.create () in
   List.iter (List.drop input 2) ~f:(parse_line g);
   Hashtbl.iter_keys g ~f:(fun k ->
-      if String.is_suffix k ~suffix:"A" then q := (k, path) :: !q);
-  while
-    not (List.for_all !q ~f:(fun (n, _) -> String.is_suffix n ~suffix:"Z"))
-  do
-    Bigint.incr ct;
-    q :=
-      List.map !q ~f:(fun (cur_node, path_seq) ->
-          let next_node, tl = Sequence.next path_seq |> Option.value_exn in
-          let l, r = Hashtbl.find_exn g cur_node in
-          let next_node = if Char.equal next_node 'L' then l else r in
-          (next_node, tl))
+      if String.is_suffix k ~suffix:"A" then Queue.enqueue q (k, path, 0));
+  while not (Queue.is_empty q) do
+    let cur_node, path_seq, local_ct = Queue.dequeue_exn q in
+    if String.is_suffix cur_node ~suffix:"Z" then
+      Queue.enqueue finished local_ct
+    else
+      let next_node, tl = Sequence.next path_seq |> Option.value_exn in
+      let l, r = Hashtbl.find_exn g cur_node in
+      let next_node = if Char.equal next_node 'L' then l else r in
+      Queue.enqueue q (next_node, tl, local_ct + 1)
   done;
-  printf "Number of steps: %s\n" (Bigint.to_string !ct)
+  printf "%s\n"
+    (Z.to_string
+       (least_common_multiple (Queue.to_list finished |> List.map ~f:Z.of_int)))
